6115ef4726e7c064283767f8d4f89ba622caf8f9
TTomaszPWR <280598@student.pwr.edu.pl>
2024-06-10 20:46:37 +0200

    feat/add-theme

---
+++ lib/theme/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorsConsts{
+
+  // Soap #FFFDFC
+  static const whiteSoap = Color(0xfffffdfc);
+
+  // Pomegranade #E43D32
+  static const orangePomegranade = Color(0xffe43d32);
+
+  // Gray #F7F7F8
+  static const greyLight = Color(0xfff7f7f8);
+
+  // Mirage #293241
+  static const blackMirage = Color(0xff293241);
+}

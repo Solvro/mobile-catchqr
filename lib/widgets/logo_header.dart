63791581cd82173f39c283e6430f732f0a9b25b4
Dmitry_Golt <dimagolt@gmail.com>
2024-07-17 11:09:45 +0200

    apply PR fixes

--- lib/widgets/logo_header.dart
+++ lib/widgets/logo_header.dart
@@ -8,8 +8,26 @@ import '../utils/context_extensions.dart';
 class LogoHeader extends SliverPersistentHeaderDelegate {
   LogoHeader({this.expandedHeight = 400, required this.onTap});
 
-  VoidCallback onTap;
-  double expandedHeight;
+  final VoidCallback onTap;
+  final double expandedHeight;
+
+  ///Most of the function uses `shrinkOffset` to know exactly how much user scrolled down from the
+  ///header. In logo position `shrinkOffset` is used to calculate fade and position on the screen,
+  ///when in background widget it's used to calculate margin, so it would touch elements below,
+  ///since logo will go up.
+
+  //General
+  double _opacityFadeVal(double shrinkOffset) => 1 - shrinkOffset / expandedHeight;
+
+  //Logo related
+  double _logoTopPosition(double shrinkOffset) => expandedHeight / 3 - shrinkOffset;
+  double _logoLeftPosition(BuildContext context) => MediaQuery.of(context).size.width / 4;
+  double _logoSize(BuildContext context) => MediaQuery.of(context).size.width / 2;
+
+  //Background related
+  double get _bgHeight => expandedHeight * 0.7;
+  double _bgBottomMargin(double shrinkOffset) =>
+      clampDouble(expandedHeight * 0.3 - shrinkOffset, 0, double.infinity);
 
   @override
   Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
@@ -18,34 +36,35 @@ class LogoHeader extends SliverPersistentHeaderDelegate {
       fit: StackFit.expand,
       children: [
         Opacity(
-          opacity: (1 - shrinkOffset / expandedHeight),
+          opacity: _opacityFadeVal(shrinkOffset),
           child: Container(
-            margin: EdgeInsets.only(
-                bottom: clampDouble(expandedHeight * 0.3 - shrinkOffset, 0, double.infinity)),
+            margin: EdgeInsets.only(bottom: _bgBottomMargin(shrinkOffset)),
             alignment: Alignment.centerLeft,
             child: Image.network( //TODO: replace placeholder.
               'https://i.cbc.ca/1.4357834.1542318134!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/old-man-senior-cell-phone-hand.jpg',
-              height: expandedHeight * 0.7,
+              height: _bgHeight,
               width: MediaQuery.of(context).size.width,
               fit: BoxFit.none,
             ),
           ),
         ),
         Positioned(
-          top: expandedHeight / 3 - shrinkOffset,
-          left: MediaQuery.of(context).size.width / 4,
+          top: _logoTopPosition(shrinkOffset),
+          left: _logoLeftPosition(context),
           child: Opacity(
-            opacity: (1 - shrinkOffset / expandedHeight),
+            opacity: _opacityFadeVal(shrinkOffset),
             child: Card(
               elevation: 10,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(34),
               ),
               child: Container(
-                height: MediaQuery.of(context).size.width / 2,
-                width: MediaQuery.of(context).size.width / 2,
+                height: _logoSize(context),
+                width: _logoSize(context),
                 decoration: BoxDecoration(
-                    gradient: ColorsConsts.toPWRGradient, borderRadius: BorderRadius.circular(34)),
+                  gradient: ColorsConsts.toPWRGradient,
+                  borderRadius: BorderRadius.circular(34),
+                ),
                 child: const Center(
                   child: Text(
                     'CatchQR',
